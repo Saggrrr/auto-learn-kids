f361afb10609eb1398f51a78b30c3f67fb396b07
Ananthasagar <ananthasagarn13@gmail.com>
2025-08-06 16:44:25 +0530

    top section of first page made

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import './MainPage.css'; 
+import homePagePic from '../assets/image/homePagePic.jpg'; 
 
 const MainPage = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Select a Vehicle</h1>
-      {/* Add vehicle cards here later */}
+    <div className='main-page'>
+        <div className='top-section'>
+            <div className='text-content'>
+                <h1>Let's learn about <br /> vehicles!</h1>
+                <p>
+                    Zoom, sail, and fly! This vehicles vocabulary list introduces kids to
+                    different modes of transportation in English, fueling their curiosity
+                    and imagination.
+                </p>
+            </div>
+            <div className='image-content'>
+                <img
+                    src={homePagePic}
+                    alt="vehicle"
+                />
+            </div>
+        </div>
+        <div className='bottom-section'>
+            <h2>Choose The mode of Learning</h2>
+            {/* This is where the vehicle cards will be rendered */}
+        </div>
     </div>
   );
 };
